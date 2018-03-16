c55fb650ef9a83d18dade711a50508ab4da0963e
nimaiwalsh <nimai.walsh@gmail.com>
2018-03-16 21:03:31 +1000

    extracted components

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -5,12 +5,111 @@ import ControlPanel from './ControlPanel';
 import { Container } from './StyledComponents';
 
 export default class GameBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardOfBlocks: [],
+      columns: 50,
+      rows: 30,
+      generationCount: 0
+    };
+
+    this.createInitialGenerationOfBlocks = this.createInitialGenerationOfBlocks.bind(this);
+    this.nextGenerationOfBlocks = this.nextGenerationOfBlocks.bind(this);
+  }
+
+  componentDidMount() {
+    this.createInitialGenerationOfBlocks();
+    this.interval = setInterval(() => this.nextGenerationOfBlocks(), 500);
+  }
+
+  handleChangeBlockLife(row, col) {
+    this.setState(prevState => {
+      const blockRef = prevState.boardOfBlocks[row][col];
+      blockRef.isAlive = !blockRef.isAlive;
+      return {
+        boardOfBlocks: prevState.boardOfBlocks
+      };
+    });
+  }
+
+  createInitialGenerationOfBlocks() {
+    //Create x rows of y blocks and set the board
+    const { columns, rows } = this.state;
+    const boardOfBlocks = [];
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      for (let j = 0; j < columns; j++) {
+        // randomly change the life of each block
+        let isAlive = Math.random() >= 0.8 ? true : false;
+        row.push({ isAlive: isAlive, row: i, column: j });
+      }
+      boardOfBlocks.push(row);
+    }
+    this.setState({ boardOfBlocks });
+  }
+
+  isNeighbourAlive(newGenerationBlocks, row, block) {
+    if (newGenerationBlocks[row] && newGenerationBlocks[row][block] && newGenerationBlocks[row][block].isAlive) {
+      //alive block
+      return true;
+    }
+    //dead block
+    return false;
+  }
+
+  nextGenerationOfBlocks() {
+    const { boardOfBlocks }= this.state;
+    const newGenerationBlocks = [];
+    //Check each current block in each row
+    boardOfBlocks.map((row, rowIndex) => {
+      let newGenerationRow = [];
+      //Count how many alive neighbours it has
+      row.map((block, blockIndex) => {
+        let aliveNeighbours = 0;
+        this.isNeighbourAlive(boardOfBlocks, rowIndex - 1, blockIndex - 1) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex - 1, blockIndex) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex - 1, blockIndex + 1) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex, blockIndex - 1) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex, blockIndex + 1) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex + 1, blockIndex - 1) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex + 1, blockIndex) && (aliveNeighbours += 1);
+        this.isNeighbourAlive(boardOfBlocks, rowIndex + 1, blockIndex + 1) && (aliveNeighbours += 1);
+        //Assign the current generation block to to the newGenerationBlocks
+        //Change its life state depending on live neighbours
+        newGenerationRow.push(boardOfBlocks[rowIndex][blockIndex])
+        if(aliveNeighbours < 2) {
+          newGenerationRow[blockIndex] = {isAlive: false, row: rowIndex, column: blockIndex};
+        }
+        if(aliveNeighbours > 3) {
+          newGenerationRow[blockIndex] = {isAlive: false, row: rowIndex, column: blockIndex};
+        }
+        if(aliveNeighbours === 3 ) {
+          newGenerationRow[blockIndex] = {isAlive: true, row: rowIndex, column: blockIndex};
+        }
+      });
+      newGenerationBlocks.push(newGenerationRow)
+    });
+    this.setState(prevState => {
+      return {
+        boardOfBlocks: newGenerationBlocks,
+        generationCount: prevState.generationCount + 1,
+      }
+    });
+  }
+
   render() {
+    const { rows, columns, boardOfBlocks } = this.state;
     return(
       <Container>
         <div>
           <ControlPanel />
-          <Board />
+          <Board 
+            rows={rows} 
+            columns={columns} 
+            boardOfBlocks={boardOfBlocks} 
+            handleClick={(col, row) => this.handleChangeBlockLife(col, row)}
+          />
         </div>
       </Container>
     )
--- src/components/backup.js
+++ src/components/backup.js
@@ -33,3 +33,45 @@
     //     }
     //   });
     // });
+
+
+
+    nextGenerationOfBlocks() {
+      const { boardOfBlocks }= this.state;
+      const newGenerationBlocks = [];
+      //Check each current block in each row
+      for (let row = 0; row < boardOfBlocks.length; row++ ){
+        let newGenerationRow = [];
+        //Count how many alive neighbours it has
+        for(let block = 0; block < boardOfBlocks[row].length; block++) {
+          let aliveNeighbours = 0;
+          this.isNeighbourAlive(boardOfBlocks, row - 1, block - 1) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row - 1, block) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row - 1, block + 1) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row, block - 1) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row, block + 1) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row + 1, block - 1) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row + 1, block) && (aliveNeighbours += 1);
+          this.isNeighbourAlive(boardOfBlocks, row + 1, block + 1) && (aliveNeighbours += 1);
+          //Assign the current generation block to to the newGenerationBlocks
+          //Change its life state depending on live neighbours
+          newGenerationRow.push(boardOfBlocks[row][block])
+          if(aliveNeighbours < 2) {
+            newGenerationRow[block] = {isAlive: false, row: row, column: block};
+          }
+          if(aliveNeighbours > 3) {
+            newGenerationRow[block] = {isAlive: false, row: row, column: block};
+          }
+          if(aliveNeighbours === 3 ) {
+            newGenerationRow[block] = {isAlive: true, row: row, column: block};
+          }
+        }
+        newGenerationBlocks.push(newGenerationRow)
+      }
+      this.setState(prevState => {
+        return {
+          boardOfBlocks: newGenerationBlocks,
+          generationCount: prevState.generationCount + 1,
+        }
+      });
+    }
